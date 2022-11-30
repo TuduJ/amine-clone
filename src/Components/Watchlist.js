afbacbbc9eb732fb9b16148e29d8935f2319dadc
JitendraTD <jitendranath.tudu@lincode.ai>
2022-11-30 11:57:34 +0530

    delete wish list added

--- src/Components/Watchlist.js
+++ src/Components/Watchlist.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { useDrop } from "react-dnd";
 
 const Watchlist = (props) => {
-  const {animeData} = props;
+  const { animeData } = props;
   const [wishListBoard, setWishListBoard] = useState([]);
   const [{ isOver }, drop] = useDrop(() => ({
     accept: "div",
@@ -14,40 +14,55 @@ const Watchlist = (props) => {
   }));
 
   const addImageToBoard = (id) => {
-    
-    const data = JSON.parse(sessionStorage.getItem("wishlist"))
-    if(data?.find((ele) => ele?.mal_id === id)){
-      return
+    const data = JSON.parse(sessionStorage.getItem("wishlist"));
+    if (data?.find((ele) => ele?.mal_id === id)) {
+      return;
     }
     const wishList = animeData?.data?.filter((anime) => id === anime?.mal_id);
     setWishListBoard((wishListBoard) => [...wishListBoard, wishList?.[0]]);
   };
 
-  useEffect(() => {
-    const data = JSON.parse(sessionStorage.getItem('wishlist'));
-    if(!data?.length)
-      setWishListBoard(data);
-  }, [])
+  const onListDelete = (id) => {
+    const wishListBoardCopy = wishListBoard.filter((data) => {
+      if(data?.mal_id !== id){
+        return data;
+      }
+    });
+     setWishListBoard(wishListBoardCopy);
+  }
 
   useEffect(() => {
-    sessionStorage.setItem('wishlist', JSON.stringify(wishListBoard));
+    const data = JSON.parse(sessionStorage.getItem("wishlist"));
+    if (!data?.length) setWishListBoard(data);
+  }, []);
+
+  useEffect(() => {
+    sessionStorage.setItem("wishlist", JSON.stringify(wishListBoard));
   }, [wishListBoard]);
 
   return (
     <>
-    <div ref={drop} className="h-100">
-      <p className="fs-2 mb-4 mt-2 text-center">This is watchlist</p>
-      {wishListBoard?.length ? (
-        wishListBoard?.map((anime) => (
-          <div key={anime?.mal_id} className="m-2 text-bg-success">
-            <p className="fs-5 text-center">{anime?.title}</p>
-          </div>
-        ))
-      ) : (
-        <>
-          <p className="text-center fs-5">Drag anime to wishlist</p>
-        </>
-      )}
+      <div ref={drop} className="h-100">
+        <p className="fs-2 mb-4 mt-2 text-center">This is watchlist</p>
+        {wishListBoard?.length ? (
+          wishListBoard?.map((anime) => (
+            <div
+              key={anime?.mal_id}
+              className="m-2 row text-bg-success"
+            >
+              <div className="col-8">
+                <p className="fs-5 text-center">{anime?.title}</p>
+              </div>
+              <div className="col-4">
+                <span className="btn" onClick={() => onListDelete(anime?.mal_id)}>delete</span>
+              </div>
+            </div>
+          ))
+        ) : (
+          <>
+            <p className="text-center fs-5">Drag anime to wishlist</p>
+          </>
+        )}
       </div>
     </>
   );
